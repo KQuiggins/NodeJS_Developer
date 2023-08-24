5db22550e3e953ab79702b650b1fbd95d8306666
KQuiggins <kashfamilyvalues@gmail.com>
2023-08-24 14:50:18 -0400

    added logging with morgan

--- nasa-project/server/src/app.js
+++ nasa-project/server/src/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router.js');
 
 const app = express();
 
 const allowedOrigins = [
-    //'http://localhost:8000',
+
     'http://localhost:3000'
 
   ];
@@ -15,8 +17,14 @@ const allowedOrigins = [
     origin: allowedOrigins,
   }));
 
+app.use(morgan('combined'));
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use(planetsRouter);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+})
 
 module.exports = app;
